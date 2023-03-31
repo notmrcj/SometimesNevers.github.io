e6b27092e72b2933f59d2caca4aafc198dfdbe53
SometimesNevers <127774312+SometimesNevers@users.noreply.github.com>
2023-03-30 23:41:52 -0700

    Update proxy.js

--- resources/proxy.js
+++ resources/proxy.js
@@ -4,7 +4,7 @@ const urlInput = document.querySelector('#url');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const url = urlInput.value;
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/' + url;
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/' + encodeURIComponent(url);
   const xhr = new XMLHttpRequest();
   xhr.open('GET', proxyUrl);
   xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
@@ -20,5 +20,4 @@ form.addEventListener('submit', (event) => {
     }
   };
   xhr.send();
-  return false;
 });
