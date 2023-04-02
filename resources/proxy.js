40a58c36f53e56688d8f6ffaa5f41035f756ebe1
Nevers <123786017+AlwaysNevers@users.noreply.github.com>
2023-04-01 17:48:16 -0700

    Update proxy.js

--- resources/proxy.js
+++ resources/proxy.js
@@ -1,23 +1,16 @@
-const form = document.querySelector('form');
-const urlInput = document.querySelector('#url');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const url = urlInput.value;
-  const proxyUrl = 'https://api.codetabs.com/v1/proxy/?quest=' + encodeURIComponent(url);
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', proxyUrl);
-  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      const response = xhr.responseText;
-      const parser = new DOMParser();
-      const htmlDoc = parser.parseFromString(response, 'text/html');
-      const body = htmlDoc.querySelector('body');
-      document.write(body.innerHTML);
-    } else {
-      console.log('Request failed.  Returned status of ' + xhr.status);
-    }
-  };
-  xhr.send();
-});
+function go(value) {
+  let iframe = document.querySelector(".iframe.active");
+  window.navigator.serviceWorker
+    .register("./sw.js", {
+      scope: __uv$config.prefix,
+    })
+    .then(() => {
+      let url = value.trim();
+      if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
+      else if (!(url.startsWith("https://") || url.startsWith("http://")))
+        url = "https://" + url;
+      //pass the encoded url to the second page
+      sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
+      location.href = "go";
+    });
+}
